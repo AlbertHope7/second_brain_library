2ebec7d45a3843a92a58c4cdd919813113bf3e22
AlbertHope7 <byiringiroalbert20@gmail.com>
2023-03-02 11:13:32 +0200

    extend book admin view

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "subtitle", "author", "isbn"]
+    list_filter = ["author"]
+    order = ["-author"]
+    search_fields = ["title", "author"]
